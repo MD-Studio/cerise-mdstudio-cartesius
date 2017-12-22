f1fb258934412093d9ded9d066886a7fe13ed83c
felipeZ <tifonzafel@gmail.com>
2017-12-22 13:58:25 +0100

    Call gromit from bash shell

--- mdstudio/gromit/call_gromit.sh
+++ mdstudio/gromit/call_gromit.sh
@@ -8,14 +8,10 @@ LIGAND_TOP="$5"
 FORCE_FIELD="$6"
 SIM_TIME="$7"
 
-function isolated_cmd {
-    module load gromacs/2016.3
-    source $CERISE_API_FILES/miniconda/bin/activate root
+module load gromacs/2016.3
+source $CERISE_API_FILES/miniconda/bin/activate root
 
-    GMXRC_FILE="/hpc/sw/gromacs-2016.3-hsw-sp/bin/GMXRC.bash"
-    GROMIT="$CERISE_API_FILES/mdstudio/github/cerise-mdstudio-cartesius/mdstudio/gromit/gromit_mpi.sh"
+GMXRC_FILE="/hpc/sw/gromacs-2016.3-hsw-sp/bin/GMXRC.bash"
+GROMIT="$CERISE_API_FILES/mdstudio/github/cerise-mdstudio-cartesius/mdstudio/gromit/gromit_mpi.sh"
 
-    $GROMIT -gmxrc $GMXRC_FILE -vsite -np 8 -f $PROTEIN_PDB -top $PROTEIN_TOP -l $LIGAND_PDB,$LIGAND_TOP -ff $FORCE_FIELD -time $SIM_TIME -lie
-}
-
-env -i isolated_cmd
+$GROMIT -gmxrc $GMXRC_FILE -vsite -np 8 -f $PROTEIN_PDB -top $PROTEIN_TOP -l $LIGAND_PDB,$LIGAND_TOP -ff $FORCE_FIELD -time $SIM_TIME -lie
